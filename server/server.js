2d3641edf09112f51200fe642e0b7b1d39fd0f5e
cbass <sebastian.wharton1@gmail.com>
2023-02-27 20:30:14 -0600

    db connected

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const productRoutes = require('./routes/products');
 
 const app = express();
@@ -14,7 +15,14 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/products', productRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-	console.log('listening on port', process.env.PORT);
-});
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
